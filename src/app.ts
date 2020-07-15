eee0c65c747bd339de2061157b01c6a7e5e06332
오충협 <tractar0213@gmail.com>
2020-07-15 16:30:37 +0900

    express 서버 삭제 및 bull 추가

--- src/app.ts
+++ src/app.ts
@@ -1,56 +1,28 @@
-import express from "express";
-import bodyParser from "body-parser";
-import methodOverride from "method-override";
-import morgan from "morgan";
 import {createConnection} from "typeorm";
-
-import { scheduler } from "./lib/scheduler";
-import { manager } from "./lib/work-manager";
-import ApplicationError from "./ApplicationError";
+import Bull from 'bull';
+import property from "../property.json"
 
 export class Application {
-  app: express.Application;
-  controllers: any[];
+  dataLoaderQueue:Bull.Queue;
 
-  constructor(controllers: any[]) {
-    this.controllers = controllers;
-    this.app = express();
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({extended: false}));
-    this.app.use(methodOverride('_method'));
-    
-    this.app.use(morgan(":remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status :res[content-length]"));
-    
-  }
+  constructor(controllers: any[]) {}
 
   setupDbAndServer = async () => {
       const conn = await createConnection().catch(error => console.log(error));
       const datasetConn = await createConnection('dataset').catch(error => console.log(error));
 
-      this.controllers.forEach((controller) => {
-        this.app.use(controller.path, controller.router);
+      this.dataLoaderQueue = new Bull('dataLoader', {
+        redis: {
+          port: property["jobqueue-redis"].port,
+          host: property["jobqueue-redis"].host
+        }
       });
 
-      this.app.use(function(req, res, next) {
-        let err = new ApplicationError(404, 'Not Found');
-        next(err);
-      });
-
-
-      await scheduler.initialize();
-
-      manager.initialize(1);
-      manager.start();
-
-      this.startServer();
-  }
-
-  startServer(): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      this.app.listen(4000, () => {
-        console.log("Server started on port: " + 4000);
-        resolve(true);
-      });
-    });
+      this.dataLoaderQueue.process(function(job, done) {
+        const data = job.data;
+        console.log(data);
+        job.progress(100);
+        done()
+      })
   }
 }
