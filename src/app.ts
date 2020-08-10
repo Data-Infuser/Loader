8bac296a0af3e862da5ce948a39e5f2f031eebfc
오충협 <tractar0213@gmail.com>
2020-08-10 15:49:58 +0900

    metaLoader progress 및 error 추가

--- src/app.ts
+++ src/app.ts
@@ -151,51 +151,60 @@ export class Loader {
       })
 
       this.metaLoaderQueue.process(async function(job, done) {
-        const serviceRepo = getRepository(Service);
-        const service = await serviceRepo.findOneOrFail({
-          relations: ["meta"],
-          where: {
-            id: job.data.serviceId
+        try {
+          job.progress(1);
+          const serviceRepo = getRepository(Service);
+          const service = await serviceRepo.findOneOrFail({
+            relations: ["meta"],
+            where: {
+              id: job.data.serviceId
+            }
+          });
+          job.progress(10);
+          if(service.status !== ServiceStatus.METASCHEDULED) {
+            const err = new Error("It's not a MetaScheduled Status");
+            done(err)
+            return;
           }
-        });
 
-        if(service.status !== ServiceStatus.METASCHEDULED) {
-          const err = new Error("It's not a MetaScheduled Status");
-          done(err)
-          return;
+          const url = job.data.url;
+          const fileName = job.data.fileName;
+          const filePath = property["upload-dist"].localPath + "/" + fileName
+          const writer = createWriteStream(filePath);
+          job.progress(20);
+          Axios({
+            method: "get",
+            url: url,
+            responseType: 'stream'
+          }).then(
+            response => {
+              job.progress(40);
+              response.data.pipe(writer);
+              let error = null;
+              writer.on('error', err => {
+                error = err;
+                writer.close();
+                done(err);
+              });
+              writer.on('close', async () => {
+                if (!error) {
+                  job.progress(70);
+                  service.status = ServiceStatus.METADOWNLOADED;
+                  service.meta.filePath = filePath;
+                  service.meta.originalFileName = fileName;
+                  await getManager().transaction("SERIALIZABLE", async transactionalEntityManager => {
+                    await transactionalEntityManager.save(service);
+                    await transactionalEntityManager.save(service.meta);
+                  });
+                  job.progress(100);
+                  done();
+                }
+              });
+            }
+          )
+        } catch (err) {
+          done(err);
         }
-
-        const url = job.data.url;
-        const fileName = job.data.fileName;
-        const filePath = property["upload-dist"].localPath + "/" + fileName
-        const writer = createWriteStream(filePath);
-        Axios({
-          method: "get",
-          url: url,
-          responseType: 'stream'
-        }).then(
-          response => {
-            response.data.pipe(writer);
-            let error = null;
-            writer.on('error', err => {
-              error = err;
-              writer.close();
-              done(err);
-            });
-            writer.on('close', async () => {
-              if (!error) {
-                service.status = ServiceStatus.METADOWNLOADED;
-                service.meta.filePath = filePath;
-                service.meta.originalFileName = fileName;
-                await getManager().transaction("SERIALIZABLE", async transactionalEntityManager => {
-                  await transactionalEntityManager.save(service);
-                  await transactionalEntityManager.save(service.meta);
-                });
-                done();
-              }
-            });
-          }
-        )
       });
   }
 }
